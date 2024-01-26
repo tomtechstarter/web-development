4a674184ccff9ae2868296b33094af30b2ffc141
SirKaktor <juliuststelldinger@web.de>
2024-01-26 10:46:58 +0100

    explained string concatination

---
+++ javascript/concat_strings.js
@@ -0,0 +1,7 @@
+// python-Logik: "Hallo!" + "Welt!" = "Hallo!Welt!"
+
+// red = 100, green = 200, blue = 255
+console.log('rgb(' + red + ', ' + green + ', ' + blue + ')');
+console.log(`rgb(${red}, ${green}, ${blue})`);
+
+console.log("rgb(100, 200, 255)");
