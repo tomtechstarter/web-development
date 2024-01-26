b659fe8fcf375a5ee70d871127a770f2fdf0e41a
SirKaktor <juliuststelldinger@web.de>
2024-01-26 10:06:29 +0100

    node-Beispiel fortgefuehrt

---
+++ javascript/grundlagen/node_project/main.js
@@ -0,0 +1,10 @@
+//Kopie von exercises/aufgabe2
+function celsiusToFahrenheit(celsius){
+    let fahrenheit = celsius * 9 / 5 + 32;
+    return fahrenheit;
+}
+
+console.log(celsiusToFahrenheit(21))    // 69.8 °F
+console.log(celsiusToFahrenheit(20))    // 68   °F
+console.log(celsiusToFahrenheit(-10))   // 14   °F
+console.log(celsiusToFahrenheit(0))     // 32   °F
