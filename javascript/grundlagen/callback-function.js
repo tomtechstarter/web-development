d5c6503d6064f745ae3468f5893a4a30305d9189
SirKaktor <juliuststelldinger@web.de>
2024-01-26 11:36:58 +0100

    callback-function explained

--- javascript/grundlagen/callback-function.js
+++ javascript/grundlagen/callback-function.js
@@ -11,3 +11,6 @@ console.log("\n\nKurz Weg...!");
 //setTimeout verlangt eine CallbackFunction, die ausgeführt wird,
 //sobald setTimeout fertig behandelt wurde (also hier: der Timer abläuft)
 setTimeout(halloWelt, 3000);
+
+//So nicht! Keinen Aufruf mitgeben.
+//setTimeout(halloWelt(), 3000);
